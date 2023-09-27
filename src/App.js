c0e09ad509c0fd1f60a7aa22eeef04e425a933a7
Clara <clarazammar4@gmail.com>
2023-09-27 12:30:52 +0300

    Fixing GithubPage - Try 1

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 export default function Website() {
     return (
         <div>
-            <Router>
+            <Router basename="/Personal-Portfolio">
                 <Routes>
-                    <Route exact path="/Personal-Portfolio/" element={<Home />} />
-                    <Route exact path="/Personal-Portfolio/resume" element={<Resume />} />
-                    <Route exact path="/Personal-Portfolio/projects" element={<Projects />} />
-                    <Route exact path="/Personal-Portfolio/contact" element={<Contact />} />
+                    <Route exact path="/" element={<Home />} />
+                    <Route exact path="/resume" element={<Resume />} />
+                    <Route exact path="/projects" element={<Projects />} />
+                    <Route exact path="/contact" element={<Contact />} />
                 </Routes>
             </Router>
         </div>
